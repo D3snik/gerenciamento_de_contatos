fae859a97808bdcbdee95087360559823c4abdca
D3snik <ryandesnik45@gmail.com>
2024-08-28 14:47:27 -0300

    Rest Controller

---
+++ src/main/java/com/gerenciamento_de_contatos/Test/controller/ContatoController.java
@@ -0,0 +1,43 @@
+package com.gerenciamento_de_contatos.Test.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gerenciamento_de_contatos.Test.entitties.Contato;
+import com.gerenciamento_de_contatos.Test.services.ContatoServices;
+
+@RestController
+@RequestMapping("/api/contato")
+public class ContatoController {
+	
+	@Autowired
+	private ContatoServices contatoService;
+	
+	@GetMapping
+	public List<Contato> listTodos() {
+		return contatoService.listarTodos();
+	}
+	
+	@PostMapping
+	public Contato adicionarContato(@RequestBody Contato contato) {
+		return contatoService.salvar(contato);
+	}
+	
+	@GetMapping("/{id}")
+	public Contato buscarPorId(@PathVariable String id) {
+		return contatoService.buscarPorid(id);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deletar(@PathVariable String id) {
+		contatoService.deletar(id);
+	}
+}
